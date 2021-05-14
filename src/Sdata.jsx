a0e1f396e7a28c314f4fe9531212e0b90cc4453f
sushilv3 <sushilv3rma@gmail.com>
2021-05-14 10:46:08 +0530

    functional component,props and more fundamentals

---
+++ src/Sdata.jsx
@@ -0,0 +1,41 @@
+
+
+const Sdata = [
+{
+sname: "BoJack Horseman",
+imgsrc:"https://static.tvtropes.org/pmwiki/pub/images/screen_shot_2014_10_03_at_15945_pm.jpg",
+title: "A Netflix Original Series",
+link:"https://www.netflix.com/in/"
+},
+
+{
+    sname: "Stranger Things",
+    imgsrc:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.iw7XJiveQMyYA-Sohx5UFQHaK-%26pid%3DApi&f=1",
+    title:"A Netflix Original Series",
+    link:"https://www.netflix.com/in/"
+    },
+
+    {
+        sname: "Ozark",
+        imgsrc:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP._dEDMIBAU2eFTle-PuMmHAAAAA%26pid%3DApi&f=1",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/"
+        },
+
+        {
+            sname: "When They See Us",
+            imgsrc:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.QOAXfWq1usrS30ahe4kFqgHaFC%26pid%3DApi&f=1",
+            title:"A Netflix Original Series",
+            link:"https://www.netflix.com/in/"
+            },
+
+            {
+                sname: "The Crown",
+                imgsrc: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.uB526EsVTH8hLonXwn57PQHaK-%26pid%3DApi&f=1",
+                title: "A Netflix Original Series",
+                link: "https://www.netflix.com/in/"
+                },
+
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card';
+import Sdata from './Sdata';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+console.log("@@@@ : ",Sdata[0]);
+
+function ncard(val){
+// console,log("ssss",val);
+  return(
+    <Card
+   imgsrc={val.imgsrc} 
+   title= {val.title}
+   sname={val.sname}
+   link ={val.link}
+   />
+  );
+}
+
+ReactDOM.render( 
+  <>
+  <h1 className="">List Of Top 5 </h1>
+  
+  {/*first way*/}
+ {/*Sdata.map(ncard)*/}
+
+   {/*second way*/}
+ {Sdata.map((val)=>{
+  return(
+  <Card
+  imgsrc={val.imgsrc} 
+  title= {val.title}
+  sname={val.sname}
+  link ={val.link}
+  />);
+ })}
+
+  </> ,  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
