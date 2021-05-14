68232cb13b69bfc8c75844c7438500f2a0c556e4
sushilv3 <sushilv3rma@gmail.com>
2021-05-14 12:40:55 +0530

    some code improvement

--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Card';
-import Sdata from './Sdata';
+import App from './App';
 
-console.log("@@@@ : ",Sdata[0]);
 
-function ncard(val){
-// console,log("ssss",val);
-  return(
-    <Card
-   imgsrc={val.imgsrc} 
-   title= {val.title}
-   sname={val.sname}
-   link ={val.link}
-   />
-  );
-}
 
-ReactDOM.render( 
-  <>
-  <h1 className="">List Of Top 5 </h1>
-  
-  {/*first way*/}
- {/*Sdata.map(ncard)*/}
-
-   {/*second way*/}
- {Sdata.map((val)=>{
-  return(
-  <Card
-  imgsrc={val.imgsrc} 
-  title= {val.title}
-  sname={val.sname}
-  link ={val.link}
-  />);
- })}
-
-  </> ,  document.getElementById('root')
-);
+ReactDOM.render(<App />,  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
