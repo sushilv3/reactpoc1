fc92ead1aba79dea5b86207f1fe36744f50dc5f8
sushilv3 <sushilv3rma@gmail.com>
2021-05-14 11:39:07 +0530

    import updated

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
+import Images from './images';
 function Card(props){
     return (
       <>
   
       <div className="cards">
         <div className="card">
-          <img src={props.imgsrc} alt="moviePic" className="card__img" />
+    {/*<img src={props.imgsrc} alt="moviePic" className="card__img" />*/}
+           <Images imgsrc={props.imgsrc} />
           <div className="card__info">
             <span className="card__category">{props.title} </span>
             <h3 className="card__title">{props.sname}  </h3>
